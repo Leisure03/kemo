f3a4d774b00ec5e3521623c301ccd637fc8dc5b7
Leisure03 <2730300269@qq.com>
2026-07-18 01:41:55 +0800

    编写将Result<KemoResponse<T>>解包为Result<T>的扩展函数，脱壳掉网络实体类

--- app/src/main/java/com/hzr/kemo/ext/ApiExt.kt
+++ app/src/main/java/com/hzr/kemo/ext/ApiExt.kt
@@ -1,13 +1,37 @@
 package com.hzr.kemo.ext
 
+import com.hzr.kemo.KemoApiException
 import com.hzr.kemo.model.DrinkListEntity
-import com.hzr.kemo.model.DrinkListResponse
+import com.hzr.kemo.model.KemoResponse
+import kotlin.math.log
 
-fun DrinkListResponse.unWarpToEntity(): List<DrinkListEntity>{
-    if(code!= 200){
-        throw RuntimeException("请求异常")
-    }
-    return data.map {
-        it
-    }
+
+fun <T> Result<KemoResponse<T>>.unwrapToEntity(): Result<T> {
+    return fold(
+        onSuccess = { response ->
+            //请求成功||服务器返回成功
+            logd("服务器响应网络成功")
+            if(response.code == 200){
+                logd("服务器响应业务成功:${response.code}")
+                val data = response.data
+                //有数据
+                if(data != null){
+                    logd("数据不为空，服务器返回数据")
+                    Result.success(data)
+                } else{
+                    logd("数据为空，抛出异常:${response.msg}")
+                    Result.failure(NullPointerException("服务器返回数据为空"))
+                }
+            }else{
+                logd("服务器响应业务错误: ${response.code}")
+                val msg = response.msg?:"未知业务错误"
+                Result.failure(KemoApiException(response.code,msg))
+            }
+        },
+        onFailure = { exception ->
+            //网络超时
+            loge(exception)
+            Result.failure(exception)
+        }
+    )
 }
